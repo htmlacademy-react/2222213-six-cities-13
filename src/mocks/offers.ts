aca704b85688aa952919c7c061e91df788e7e5e6
Александр <savinovalexsandr93z@gmail.com>
2023-07-30 18:47:15 +0300

    изменяет моковые данные и добавляет координаты

--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -1,51 +1,51 @@
-import {TOffer} from '../types/offer-type';
+import {TOffers} from '../types/offer-type';
 
-export const offers: TOffer[] = [
+export const offers: TOffers = [
   {
     id: crypto.randomUUID(),
     title: 'The Joshua Tree House',
-    type: 'hotel',
-    price: 178,
-    previewImage: 'https://13.design.pages.academy/static/hotel/10.jpg',
+    type: 'house',
+    price: 720,
+    previewImage: 'https://13.design.pages.academy/static/hotel/3.jpg',
     city: {
-      name: 'Paris',
+      name: 'Amsterdam',
       location: {
-        latitude: 48.85661,
-        longitude: 2.351499,
+        latitude: 52.37454,
+        longitude: 4.897976,
         zoom: 13
       }
     },
     location: {
-      latitude: 48.868610000000004,
-      longitude: 2.342499,
+      latitude:  52.3909553943508,
+      longitude: 4.85309666406198,
       zoom: 16
     },
     isFavorite: false,
     isPremium: true,
-    rating: 1.7
+    rating: 1.4
   },
   {
     id: crypto.randomUUID(),
-    title: 'Tile House',
+    title: 'Wood and stone place',
     type: 'hotel',
-    price: 458,
-    previewImage: 'https://13.design.pages.academy/static/hotel/9.jpg',
+    price: 422,
+    previewImage: 'https://13.design.pages.academy/static/hotel/19.jpg',
     city: {
-      name: 'Brussels',
+      name: 'Amsterdam',
       location: {
-        latitude: 50.846557,
-        longitude: 4.351697,
+        latitude: 52.37454,
+        longitude: 4.897976,
         zoom: 13
       }
     },
     location: {
-      latitude: 50.854557,
-      longitude: 4.364697,
+      latitude: 52.3609553943508,
+      longitude: 4.85309666406198,
       zoom: 16
     },
     isFavorite: false,
-    isPremium: true,
-    rating: 2.4
+    isPremium: false,
+    rating: 4.8
   },
   {
     id: crypto.randomUUID(),
@@ -62,8 +62,8 @@ export const offers: TOffer[] = [
       }
     },
     location: {
-      latitude: 52.361540000000005,
-      longitude: 4.883976,
+      latitude: 52.3909553943508,
+      longitude: 4.929309666406198,
       zoom: 16
     },
     isFavorite: false,
@@ -72,25 +72,26 @@ export const offers: TOffer[] = [
   },
   {
     id: crypto.randomUUID(),
-    title: 'House in countryside',
+    title: 'Amazing and Extremely Central Flat',
     type: 'room',
-    price: 187,
-    previewImage: 'https://13.design.pages.academy/static/hotel/20.jpg',
+    price: 223,
+    previewImage: 'https://13.design.pages.academy/static/hotel/10.jpg',
     city: {
-      name: 'Hamburg',
+      name: 'Amsterdam',
       location: {
-        latitude: 53.550341,
-        longitude: 10.000654,
+        latitude: 52.37454,
+        longitude: 4.897976,
         zoom: 13
       }
     },
     location: {
-      latitude: 53.570341000000006,
-      longitude: 9.975654,
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
       zoom: 16
     },
     isFavorite: false,
-    isPremium: false,
-    rating: 4
+    isPremium: true,
+    rating: 2.9
   }
 ];
+
